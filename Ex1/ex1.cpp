d65116b4dc029d506a0487bbf1cfe98eedc81b7e
Youssef Bakouny <Youssef.Bakouny@usj.edu.lb>
2024-10-16 11:50:17 +0300

    Test Ex1 Partie 2 using main

--- Ex1/ex1.cpp
+++ Ex1/ex1.cpp
@@ -5,13 +5,20 @@
 #include "functions.h"
 using namespace std; 
 
-
-
-
 int main() {
-    vector<double> v = create(3);
-    cout << "v = ";
-    affiche(v);
+    List l1 = create(5);
+    cout << "l1 = ";
+    affiche(l1);
+    cout << endl;
+
+    List l2 = reverse(l1);
+    cout << "l2 = ";
+    affiche(l2);
+    cout << endl;
+
+    List l = concat(l1, l2);
+    cout << "l = ";
+    affiche(l);
     cout << endl;
 
     return 0;
